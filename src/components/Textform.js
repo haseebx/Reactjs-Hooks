135f02e472875a95425eb9f5f04e0837e4ac8c31
haseeb <hasbm125@gmail.com>
2022-10-22 12:50:02 +0500

    first commit

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -10,28 +10,43 @@ export default function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText);
   };
+  const handleClearClick = () => {
+    let newText = '';
+    setText(newText);
+  };
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
   return (
-    <div>
-      <h1>{props.headding}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="mybox"
-          value={text}
-          rows="10"
-          onChange={handleOnChange}
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.headding}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="mybox"
+            value={text}
+            rows="10"
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <div className="btn btn-primary" onClick={handleUpClick}>
+          Convert to UpperCase
+        </div>
+        <div className="btn btn-info ml-3" onClick={handleLoClick}>
+          Convert to LowerCase
+        </div>
+        <div className="btn btn-danger ml-3" onClick={handleClearClick}>
+          Clear
+        </div>
       </div>
-      <div className="btn btn-primary" onClick={handleUpClick}>
-        Convert to UpperCase
+
+      <div className="container my-3">
+        <p>
+          {text.split(' ').length} Words and {text.length} Characters
+        </p>
       </div>
-      <div className="btn btn-danger ml-3" onClick={handleLoClick}>
-        Convert to LowerCase
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,47 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends Component {
-  state = {
+export default function Counter() {
+  //class Counter extends Component {
+  const state = {
     //created Object
     count: 0,
     //  tags: ['tag1', 'tag2', 'tag3'],
   };
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+  const handleIncrement = () => {
+    this.setState({ count: state.count + 1 });
   };
-  handleSetZero = () => {
+  const handleSetZero = () => {
     this.setState({ count: 0 });
   };
-  render() {
-    return (
-      <div style={{ marginLeft: 850, marginTop: 250 }}>
-        <span className={this.getbadgerClasse()} style={{ fontSize: 20 }}>
-          {this.formatcounter()}
-        </span>
-        <button
-          onClick={this.handleIncrement}
-          className="btn btn-secondary btn-lg"
-        >
-          Inc
-        </button>
-        <button
-          onClick={this.handleSetZero}
-          className="btn btn-danger ml-3 btn-lg"
-        >
-          Clear
-        </button>
-      </div>
-    );
-  }
-  getbadgerClasse = () => {
+  // render() {
+  const getbadgerClasse = () => {
     let classes = ' badge m-3 badge-';
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += state.count === 0 ? 'warning' : 'primary';
     return classes;
   };
-  formatcounter = () => {
-    const { count } = this.state;
+  const formatcounter = () => {
+    const { count } = state;
     return count === 0 ? 'Zero' : count; //if statement
   };
+  return (
+    <>
+      <div style={{ marginLeft: 850, marginTop: 250 }}>
+        <span className={getbadgerClasse()} style={{ fontSize: 20 }}>
+          {formatcounter()}
+        </span>
+        <button
+          onClick={handleIncrement()}
+          className="btn btn-secondary btn-lg"
+        >
+          {this.props.ins}
+        </button>
+        <button
+          onClick={handleSetZero()}
+          className="btn btn-danger ml-3 btn-lg"
+        >
+          {this.props.cls}
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Counter;
+//export default Counter;
